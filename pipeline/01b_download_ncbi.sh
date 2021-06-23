f0901ffddd15acfe530a2375f879b8b2039be002
Jason Stajich <jasonstajich.phd@gmail.com>
2021-06-22 22:13:06 -0700

    fix ids for uniqueness

--- pipeline/01b_download_ncbi.sh
+++ pipeline/01b_download_ncbi.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash -l
+B0;95;0c#!/usr/bin/bash -l
 #SBATCH -p short
 
 ACCFILE=lib/NCBI_accessions.csv
@@ -7,7 +7,7 @@ TARGETDIR=pep
 TEMPDIR=NCBI
 mkdir -p $TEMPDIR $TARGETDIR
 IFS=,
-cat $ACCFILE | grep saturninu | while read SPECIES STRAIN ACCESSION
+cat $ACCFILE | while read SPECIES STRAIN ACCESSION
 do
     STEM=$(echo -n "$SPECIES $STRAIN" | perl -p -e 's/\s+/_/g')
     
@@ -17,5 +17,5 @@ do
     THREE=$(echo $ACCESSION | cut -d_ -f2 | awk '{print substr($1,7,3)}')
     echo "$PRE/$ONE/$TWO/$THREE/$ACCESSION/${ACCESSION}_translated_cds.faa.gz"
     curl -o $TEMPDIR/$STEM.aa.fasta.gz $URLBASE/$PRE/$ONE/$TWO/$THREE/$ACCESSION/${ACCESSION}_translated_cds.faa.gz
-    pigz -dc $TEMPDIR/$STEM.aa.fasta.gz | perl -p -e 'if ( ! s/>lcl\|(\S+)\s+.*\[locus_tag=(([^_]+)_([^\]]+))\]/>$3|$2 $1/ ) { s/^\-// }' > $TARGETDIR/$STEM.aa.fasta
+    pigz -dc $TEMPDIR/$STEM.aa.fasta.gz | ./scripts/process_ncbi_ids.pl > $TARGETDIR/$STEM.aa.fasta
 done
--- scripts/process_ncbi_ids.pl
+++ scripts/process_ncbi_ids.pl
@@ -6,9 +6,13 @@ my %seen;
 
 while(<>) {
     # replace locus tag
-    if ( s/>lcl\|(\S+)\s+.*\[locus_tag=(([^_]+)_([^\]]+))\]/>$3|$2 $1/ ) { 
-	if ( $seen{$2}++ > 1 ) {
-	    $_ = sprintf(">%s.%d %s",$2,$seen{$2},$1)
+    if ( s/>lcl\|(\S+)\s+.*\[locus_tag=(([^_]+)_([^\]]+))\]\s+(.+)/>$3|$2 $1/ ) { 
+	my $pref = $3;
+	my $id = $2;
+	my $longname = $1;
+	$seen{$id}++;
+	if ( $seen{$id} > 1 ) {
+	    $_ = sprintf(">%s|%s.%d %s\n",$pref,$id,$seen{$id},$longname);
 	} 
     } else {
 	s/^\-//; # deal with mis-specified partial genes for proteins
